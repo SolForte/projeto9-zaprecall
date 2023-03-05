5d9ae6783574ef813766d9e6d1b7bec422f61f84
SolForte <mateusbrandao1234@gmail.com>
2023-03-05 05:19:38 -0300

    Feat: botoes de responder (sem funcionalidade)

--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -6,32 +6,79 @@ import { useState } from "react";
 export default function Flashcard(props){
     const {question, answer, numero} = props;
 
-    const altPlayDescription = `Abrir pergunta número ${numero}`
+    const altPlayDescription = `Ativar pergunta número ${numero}`
     const altFlipDescription = `Virar pergunta número ${numero}`
 
     const [inativa, setInativa] = useState(true)
+    const [virada, setVirada] = useState(false)
 
     function ativar(){
         setInativa(false);
     }
+    function virar(){
+        setVirada(true);
+    }
 
-    if (inativa === true){
+    if (inativa === true && virada === false){
         return(
             <Inativa>
-                <p>Pergunta número {numero}</p>
+                <p>Pergunta {numero}</p>
                 <img src={seta_play} alt={altPlayDescription} onClick={ativar}/>
             </Inativa>)
-        }
-    if (inativa === false){
+    } else if (inativa === false && virada === false){
         return(
             <Ativa>
                 <p>{question}</p>
-                <img src={seta_virar} alt={altFlipDescription}/>
+                <img src={seta_virar} alt={altFlipDescription} onClick={virar}/>
             </Ativa>
+        )
+    } else if (inativa === false && virada === true){
+        return(
+            <Virada>
+                <p>{answer}</p>
+                <Botoes>
+                    <button>Não lembrei</button>
+                    <button>Quase não lembrei</button>
+                    <button>Zap!</button>
+                </Botoes>
+            </Virada>
         )
     }
     
 }
+
+const Botoes = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    button{
+        width: 85.17px;
+        height: 37.17px;
+        border-radius: 5px;
+        text-decoration: none;
+        border: none;
+    }
+`
+
+const Virada = styled.div`
+    width: 300px;
+    height: 131px;
+    background: #FFFFD5;
+    box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+    border-radius: 5px;
+    display: flex;
+    flex-direction: column;
+    p{
+        font-family: 'Recursive', cursive;
+        font-style: normal;
+        font-weight: 400;
+        font-size: 18px;
+        line-height: 22px;
+        color: #333333;
+        margin: 18px 15px 18px 15px;
+    }
+`
 
 const Ativa = styled.div`
     width: 299px;
